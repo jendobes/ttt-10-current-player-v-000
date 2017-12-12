0ac5318563deb52cbd6f63f5d574548460e412b1
Jennifer Dobak <jdobak1@gmail.com>
2017-12-12 19:46:09 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,11 +1,14 @@
+require 'pry'
+
 def turn_count(board)
-counter = 0
-board.each do |value|
-  if value == "X" || value == "O"
-    counter += 1
-    counter
-end
-end
+  counter = 0
+  board.each do |value|
+    if value == "X" || value == "O"
+      binding.pry
+      counter += 1
+    end
+  end
+  counter
 end
 
 def current_player(board)
